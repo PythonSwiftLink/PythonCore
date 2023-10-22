4e9e4a596b4a6b56cc0e4d95abf9491d77508616
pythonswiftlink <pythonswiftlink@gmail.com>
2023-10-22 18:58:46 +0000

    Update from https://github.com/PythonSwiftLink/PythonCoreBuilder/commit/3815f7772b7c70ccfc003524c36b6535a1346d11

--- Package.swift
+++ Package.swift
@@ -32,6 +32,6 @@ let package = Package(
         		.linkedLibrary("sqlite3"), 
         	]
     	), 
-    	.binaryTarget(name: "Python", url: "https://github.com/PythonSwiftLink/PythonCore/releases/download/310.0.0/Python.zip", checksum: "cc0eff4cdce118c0de6636bbb17c260bd4dba06a9473e53662eff07f01aacd26"), 
+    	.binaryTarget(name: "Python", url: "https://github.com/PythonSwiftLink/PythonCore/releases/download/310.0.0/Python.zip", checksum: "593931519129ce602cc51cad190f294cf39183450e0215ffa4e724391a62c5bb"), 
     ]
 )
