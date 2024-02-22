7e9f7be95c9e41b195e44b236dd081a607e6b15e
psychowasp <psychowaspx@gmail.com>
2024-02-22 11:14:47 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,54 +4,46 @@
 import PackageDescription
 
 let package = Package(
-    name: "PythonCore", 
-    platforms: [.iOS(.v13), .macOS(.v11)], 
-    products: [
-    	.library(
-        	name: "PythonCore", 
-        	targets: [
-        		"PythonCore", 
-        				"Python", 
-        				"PythonExtra"
-        	]
-    	), 
-    	.library(
-        	name: "PythonCore", 
-        	targets: [
-        		"PythonCore", 
-        	"PythonExtra", 
-        	"Python"
-        	]
-    	), 
-    ], 
-    dependencies: [
-    ], 
-    targets: [
-    	.target(
-        	name: "PythonCore", 
-        	dependencies: [
-        		"Python", 
-        	], 
-        	resources: [
-        	], 
-        	linkerSettings: [
-        		.linkedLibrary("bz2"), 
-        		.linkedLibrary("z"), 
-        		.linkedLibrary("ncurses"), 
-        		.linkedLibrary("sqlite3"), 
-        	]
-    	), 
-    			.target(
-        				name: "PythonExtra", 
-        				dependencies: [
-        					"Python"
-        				]
-    				   
-    			), 
-    		.target(
-        			name: "TestPython", 
-        			dependencies: ["PythonCore"]
-    		), 
-    	.binaryTarget(name: "Python", url: "https://github.com/PythonSwiftLink/PythonCore/releases/download/311.0.2/Python.zip", checksum: "410d57419f0ccbc563ab821e3aa241a4ed8684888775f4bdea0dfc70820b9de6"), 
-    ]
+	name: "PythonCore", 
+	platforms: [.iOS(.v13), .macOS(.v11)], 
+	products: [
+		.library(
+			name: "PythonCore", 
+			targets: [
+				"PythonCore", 
+				"Python", 
+				"PythonExtra"
+			]
+		), 
+	], 
+	dependencies: [
+	], 
+	targets: [
+		.target(
+			name: "PythonCore", 
+			dependencies: [
+				"Python", 
+			], 
+			resources: [
+			], 
+			linkerSettings: [
+				.linkedLibrary("bz2"), 
+					.linkedLibrary("z"), 
+					.linkedLibrary("ncurses"), 
+					.linkedLibrary("sqlite3"), 
+			]
+		), 
+			.target(
+				name: "PythonExtra", 
+				dependencies: [
+					"Python"
+				]
+				
+			), 
+			.target(
+				name: "TestPython", 
+				dependencies: ["PythonCore"]
+			), 
+			.binaryTarget(name: "Python", url: "https://github.com/PythonSwiftLink/PythonCore/releases/download/311.0.2/Python.zip", checksum: "410d57419f0ccbc563ab821e3aa241a4ed8684888775f4bdea0dfc70820b9de6"), 
+	]
 )
