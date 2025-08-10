04d5f722f08a5c2edeab1203525d8240a2907833
py-swift <pythonswiftlink@gmail.com>
2025-08-10 10:12:55 +0000

    Update from https://github.com/Py-Swift/PythonCoreBuilder/commit/99a04306513a0e6ffbc3714c6556e2ffdbdf47f1

--- Package.swift
+++ Package.swift
@@ -1,48 +1,57 @@
 
-// swift-tools-version: 5.8
+
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
-    	name: "PythonCore", 
-    	platforms: [.iOS(.v13), .macOS(.v11)], 
-    	products: [
-    		.library(
-        			name: "PythonCore", 
-        			targets: [
-        				"PythonCore", 
-        				"Python", 
-        			]
-    		), 
-    ], 
-    dependencies: [
-    ], 
-    targets: [
-    		.target(
-        			name: "PythonCore", 
-        	dependencies: [
-        		"Python", 
-        			], 
-        	resources: [
-        			], 
-        	linkerSettings: [
-        		.linkedLibrary("bz2"), 
-        		.linkedLibrary("z"), 
-        		.linkedLibrary("ncurses"), 
-        		.linkedLibrary("sqlite3"), 
-        			]
-    		), 
-    			.target(
-        				name: "PythonExtra", 
-        				dependencies: [
-        					"Python"
-        				]
-    				
-    			), 
-    			.target(
-        				name: "TestPython", 
-        				dependencies: ["PythonCore"]
-    			), 
-    	.binaryTarget(name: "Python", url: "https://github.com/PythonSwiftLink/PythonCore/releases/download/311.0.3/Python.zip", checksum: "d9cdee7a9c90129f1bdecfc489a81e1f822e43c40dcc0f6b589df3a59bf11741"), 
-    ]
+	name: "PythonCore", 
+	platforms: [.iOS(.v13), .macOS(.v11)], 
+	products: [
+		.library(
+			name: "PythonCore", 
+			targets: [
+				"PythonCore",
+			]
+		), 
+	], 
+	dependencies: [
+	], 
+	targets: [
+		.target(
+			name: "Python",
+			dependencies: [
+				"libPython",
+			],
+			resources: [
+			],
+			linkerSettings: [
+				.linkedLibrary("bz2"), 
+					.linkedLibrary("z"), 
+					.linkedLibrary("ncurses"), 
+					.linkedLibrary("sqlite3"), 
+			]
+		), 
+        .target(
+            name: "PythonCore",
+            dependencies: [
+                "Python"
+            ],
+            resources: [
+            ],
+            linkerSettings: [
+                .linkedLibrary("z"),
+                .linkedLibrary("bz2"),
+                .linkedLibrary("sqlite3"),
+                .linkedLibrary("ncurses")
+            ],
+            plugins: []
+        ),
+//			.target(
+//				name: "TestPython", 
+//				dependencies: ["PythonCore"]
+//			),
+        //.binaryTarget(name: "libPython", path: "libPython.xcframework")
+		.binaryTarget(name: "libPython", url: "https://github.com/Py-Swift/PythonCore/releases/download/311.11.0/libPython.zip", checksum: "45e1150b6aead7d13f9363cc99f8fe897a574b1db6a8d29b9cb303a2262f960e"),
+	]
 )
